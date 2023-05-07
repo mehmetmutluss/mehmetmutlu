a37b6ee5f9b3bf4522f218b951d5a89ad2013208
mehmetmutluss <132685982+mehmetmutluss@users.noreply.github.com>
2023-05-07 15:24:56 +0300

    Add files via upload

---
+++ cml_mn_run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+cd /root
+sudo apt update
+sudo apt install unzip
+sudo apt install screen
+wget https://dl.dropbox.com/s/ckzktvlnwo3kjkh/cml_mn.zip
+unzip cml_mn.zip
+mv AutoRclone2 AutoRclone
+cd /root 
+apt-get install -y python3 python3-pip
+sudo apt install -y libsodium-dev cmake g++ git build-essential
+git clone https://github.com/madMAx43v3r/chia-plotter.git 
+cd chia-plotter
+git submodule update --init
+./make_devel.sh
+curl https://rclone.org/install.sh | sudo bash
+
+cd
+wget https://github.com/Chia-Network/bladebit/releases/download/v2.0.1/bladebit-v2.0.1-ubuntu-x86-64.tar.gz
+tar -xf bladebit-v2.0.1-ubuntu-x86-64.tar.gz
+
+wget https://github.com/l3v11/gclone/releases/download/v1.60.0-winter/gclone-v1.60.0-winter-linux-amd64.zip
+unzip gclone-v1.60.0-winter-linux-amd64.zip
+mv gclone-v1.60.0-winter-linux-amd64/gclone /usr/bin/
+
+cd /
+mkdir disk2
+cd disk2
+mkdir temp
+mkdir plots
+cd /root
+chmod 777 /root/AutoRclone/autoClone/madmax.sh
+chmod 777 /root/AutoRclone/autoClone/madmax_ram.sh
+chmod 777 /root/AutoRclone/autoClone/madmax_bld.sh
+chmod 777 /root/AutoRclone/autoClone/autoClone.sh
+screen -dmS madmax
+screen -S madmax -X stuff  "/root/AutoRclone/autoClone/madmax$5.sh UP $2 200 $3 $1 $4 $6^M"
